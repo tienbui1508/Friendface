5174bd2dafed142b5f1a41da24bd91919c850168
tienbui <tienbui1508@gmail.com>
2023-06-21 20:42:28 +0930

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Tien Bui on 21/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
